bf0d619b20a6c7b451a28e33dbf551ae5eb070ff
Maria Ma <i.am.maria.ma@gmail.com>
2018-11-05 18:22:07 -0800

    fixed my other mistake
    fixed a mistake that i'm not too sure how i introduced.

--- 01_data_import.R
+++ 01_data_import.R
@@ -3,14 +3,12 @@
 
 # Load Packages -----------------------------------------------------------
 if (!require(here)) install.packages("here")
-library(here) 
+library(here)
 source('~/Documents/GitHub/kivu-ebola-conflict/00_load_packages.R')
 # list.files("data") # List files in '/data'
 
 # Import EVD Outbreak Data ------------------------------------------------
-d_evd <- read.csv(file = here::here("data", "Data_ Ebola Outbreak, North Kivu, Democratic Republic of the Congo (DRC) - August 2018 - MOH-By-Health-Zone.csv"))
-
-# note from maria: added here::here because it wasn't quite working on my side without it 
+d_evd <- readr::read_csv(file = here::here("data", "Data_ Ebola Outbreak, North Kivu, Democratic Republic of the Congo (DRC) - August 2018 - MOH-By-Health-Zone.csv"))
 
 # Import DRC Conflict Data ------------------------------------------------
-d_drc <- read.csv(file = here::here("data", "2018-08-01-2018-10-16-Democratic_Republic_of_Congo.csv"))
+d_drc <- readr::read_csv(file = here::here("data", "2018-08-01-2018-10-16-Democratic_Republic_of_Congo.csv"))
