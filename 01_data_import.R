d5e8fb40670df51be2b905c21b44c90a443a79e5
Maria Ma <i.am.maria.ma@gmail.com>
2018-11-26 15:21:00 -0800

    identify missing data sources + data update

--- 01_data_import.R
+++ 01_data_import.R
@@ -11,4 +11,4 @@ source('~/Documents/GitHub/kivu-ebola-conflict/00_load_packages.R')
 d_evd <- readr::read_csv(file = here::here("data", "Data_ Ebola Outbreak, North Kivu, Democratic Republic of the Congo (DRC) - August 2018 - MOH-By-Health-Zone.csv"))
 
 # Import DRC Conflict Data ------------------------------------------------
-d_drc <- readr::read_csv(file = here::here("data", "2018-08-01-2018-10-16-Democratic_Republic_of_Congo.csv"))
+d_drc <- readr::read_csv(file = here::here("data", "2018-08-01-2018-11-26-Democratic_Republic_of_Congo.csv"))
