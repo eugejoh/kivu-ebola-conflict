2a6f309edf5e2ad5e989d0806ae1a55507514452
eugejoh <ejoh90@gmail.com>
2018-12-27 10:09:05 -0500

    updated data import
    added the removal of first row in outbreak data into `01_data_import.R`

--- R/ebola_descriptors.R
+++ R/ebola_descriptors.R
@@ -1,10 +1,12 @@
 #source(here::here("01_data_import.R"))
 # Fixing Data Types -------------------------------------------------------
-evd <- d_evd[-1,] 
 evd[1:2] <- lapply(evd[1:2],as.Date) 
-#sorry for hard coding, i'm sure there's a better way to do this
+#sorry for hard coding, i'm sure there's a better way to do this #EJ I think this way is really clever, didn't know about this method!
 evd[6:16] <- sapply(evd[6:16],as.numeric)
 
+# dplyr method 
+# evd %>% mutate_at(vars(1:2), as.Date)
+# evd %>% mutate_at(vars(6:16), as.numeric)
 
 # Descriptions ------------------------------------------------------------
 unique_provinces <- unique(evd$province)
@@ -23,6 +25,11 @@ report_date_range <- paste(format(min(evd$report_date), "%B %d %Y"), "to",
 #identifying which rows have missing data
 nas <- d_evd[!complete.cases(d_evd),]
 
+# identifying how many missing values in each row
+na_sum <- sapply(d_evd, function(i) sum(is.na(i)))
+
+na_index <- lapply(d_evd, function(i) which(is.na((i))))
+na_index <- na_index[unlist(lapply(na_index, function(x) length(x) > 0))] #remove ones with no missing
 
 # Quick epicurve ----------------------------------------------------------
 
