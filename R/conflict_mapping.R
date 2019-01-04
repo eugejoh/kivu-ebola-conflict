b581d1f3d8c4cf2420a85f2cdc47aeb6437adc33
Maria Ma <i.am.maria.ma@gmail.com>
2019-01-03 20:52:57 -0800

    put each conflict data point within a healthzone
    every conflict data point is now tagged with its associated health zone.
    
    beni_conflict just shows an example.

--- R/conflict_mapping.R
+++ R/conflict_mapping.R
@@ -37,10 +37,30 @@ ggplot(spatialConflict) + theme_void() +
 #anim_save("kivu_conflict.gif")
 #uncomment for gif. i commented it out because it takes forever and i didn't want to accidentally trigger it again.
 
-
 # theme_void is necessary to make it stop randomly throwing a "polygon edge not found" error - 
 # "polygon edge not found" is not reproducible, but github suggests that it's related to the degree symbol
 # removing axis ticks entirely seems to fix it. 
 
 
 # there's that one weird point which is ... probably not in kivu. dunno what to do with it. 
+
+conflictInBorders <- as.data.frame(st_join(spatialConflict, drcBorders, join = st_intersects))
+# is... that it?? This feels like it was easier than it should have been. 
+
+# next part is commented out because it doesn't actually make sense, but it seemed like
+# an intuitive next step atm. i'm leaving this in here so no one else thinks it's a good idea. 
+# - doing this breaks how sf works, and also it's kind of weird anyway. 
+#smallBorders <- drcBorders %>% select(ADM2_ALTNA, geometry)
+#joined <- left_join(conflictInBorders, smallBorders, by = "ADM2_ALTNA")
+#joined <- joined %>% select(event_date, admin2, fatalities, ADM2_ALTNA, geometry.x, geometry.y) %>% 
+#  rename(conflict_loc= geometry.x, border=geometry.y)
+
+# quick check to see what each of the adm2 borders looks like 
+ggplot(subset(conflictInBorders, ADM2_NAME == "BENI")) + theme_void() + 
+  geom_sf(data = subset(drcBorders, ADM2_NAME == "BENI"), aes(fill = ADM2_NAME)) + 
+  geom_sf(aes(size = fatalities), alpha = .5, show.legend = "point") 
+#ggsave("beni_conflict.png")
+
+conflictInBorders <- conflictInBorders %>% select(event_date, fatalities, ADM1_NAME, ADM2_ALTNA, geometry) %>% drop_na
+# conflictInBorders now is a record of conflict events that happened within each health zone.
+# everything that was outside of Ituri or Nord Kivu was dropped. 
